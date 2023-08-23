a091d998634cb5977dc400a2fdd3c493198e9bd3
afonsohfontes <afonsohfontes@gmail.com>
2023-08-23 08:24:48 +0200

    afonsos changes

--- framework/bin/gen_tests.pl
+++ framework/bin/gen_tests.pl
@@ -157,9 +157,9 @@ my $CRITERION = $cmd_opts{C};
 #my $EVO_OUTPUT = "ExecutionTimeCoverage,BranchCoverageBitString,BranchBitstringTimeline,OnlyBranchCoverageBitString,OnlyBranchBitstringTimeline,PrivateMethodCoverageTimeline,PrivateMethodCoverageBitString,PrivateMethodBitstringTimeline,ExceptionCoverage,ExceptionCoverageBitString,ExceptionBitstringTimeline";
 #my $EVO_OUTPUT = "BranchCoverageBitString,BranchBitstringTimeline,PrivateMethodCoverageTimeline,PrivateMethodCoverageBitString,PrivateMethodBitstringTimeline,ExceptionCoverage,ExceptionCoverageBitString,ExceptionBitstringTimeline";
 #my $EVO_OUTPUT = "BranchCoverageBitString,BranchBitstringTimeline,OnlyBranchCoverageBitString,OnlyBranchBitstringTimeline,PrivateMethodCoverageTimeline,PrivateMethodCoverageBitString,PrivateMethodBitstringTimeline,ExceptionCoverage,ExceptionCoverageBitString,ExceptionBitstringTimeline";
-my $EVO_OUTPUT = "BranchCoverageBitString"
+my $EVO_OUTPUT = "BranchCoverageBitString";
 if ($CRITERION ne "BRANCH") {
-    my $EVO_OUTPUT = "ExceptionCoverage"
+    $EVO_OUTPUT = "ExceptionCoverage";
 }
 
 #if ((index($CRITERION, "PRIVATEMETHOD") != -1) or (index($CRITERION, "privatemethod") != -1) or (index($CRITERION, "PrivateMethod") != -1)) {
