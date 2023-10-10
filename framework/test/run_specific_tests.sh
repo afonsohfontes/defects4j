177b2f5e9347a3f8c82145e1e5f55c5d9c4c7c48
afonsohfontes <afonsohfontes@gmai.com>
2023-10-10 14:23:46 +0200

    afonso changes

---
+++ framework/test/run_specific_tests.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Read the list of configurations line by line
+while IFS=',' read -r project bug criterion budget extra_trials; do
+    # Skip the header line
+    if [ "$project" != "Project" ]; then
+        # Run specific_tests.sh for each configuration
+        echo "Running specific test for Project: $project, Bug: $bug, Criterion: $criterion, Budget: $budget, Extra_Trials: $extra_trials"
+        sudo ./specific_tests.sh -p "$project" -b "$bug" -c "$criterion" -o "$budget" -t "$extra_trials"
+    fi
+done < configurations_to_run.txt
